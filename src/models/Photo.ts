5a1886ec285067dd42b259d237c5719a030aa9ce
Ebedon44 <alexbedon44@gmail.com>
2022-07-19 11:36:55 -0500

    Api de Imagenes con Mongo

---
+++ src/models/Photo.ts
@@ -0,0 +1,13 @@
+import { Schema, model, Document } from 'mongoose'
+
+const schema = new Schema({
+    
+    imagePath: String
+});
+
+export interface IPhoto extends Document {
+    
+    imagePath: string;
+}
+
+export default model<IPhoto>('Photo', schema);
